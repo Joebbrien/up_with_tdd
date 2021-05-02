aa0c50f42d9139610bfe5fbd2f93ee912e9c32cb
Joebbrian <job.bundabunda@gmail.com>
2021-05-02 19:29:44 +0200

    Added CRUD routes to user controller

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -4,8 +4,35 @@ const httpResponses = require("../utils/httpResponses");
 
 exports.createUser = (req,res,next)=>{
   try {
-  res.send(httpResponses.success, message:"User created successfully");
+  res.send(httpResponses.create, message:"User created successfully");
   } catch (error) {
-    res.send({status:})
+    console.log(error.stack);
+    res.send({status:httpResponses.serverError,message:"Something has gone wrong"});
+  }
+}
+
+exports.listUsers = (req,res,next)=>{
+  try {
+  res.send(httpResponses.success, message:"List of users");
+  } catch (error) {
+    console.log(error.stack);
+    res.send({status:httpResponses.serverError,message: "Something has gone wrong"})
+  }
+}
+exports.updateUser = (req,res,next)=>{
+  try {
+  res.send(httpResponses.success, message:"User updated successfully");
+  } catch (error) {
+    console.log(error.stack);
+    res.send({status:httpResponses.serverError,message: "Something has gone wrong"})
+  }
+}
+
+exports.deleteUser = (req,res,next)=>{
+  try {
+  res.send(httpResponses.success, message:"User deleted successfully");
+  } catch (error) {
+    console.log(error.stack);
+    res.send({status:httpResponses.serverError,message: "Something has gone wrong"})
   }
 }
